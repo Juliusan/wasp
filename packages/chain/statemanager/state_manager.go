52b02efcddbb4d4d12ab5f384a8080efddfc974c
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2022-10-25 11:18:35 +0300

    State manager network and GPA usage

--- packages/chain/statemanager/state_manager.go
+++ packages/chain/statemanager/state_manager.go
@@ -2,6 +2,7 @@ package statemanager
 
 import (
 	"context"
+	"time"
 
 	"github.com/iotaledger/hive.go/core/kvstore"
 	"github.com/iotaledger/hive.go/core/logger"
@@ -13,8 +14,11 @@ import (
 	"github.com/iotaledger/wasp/packages/chain/statemanager/smUtils"
 	"github.com/iotaledger/wasp/packages/cryptolib"
 	"github.com/iotaledger/wasp/packages/gpa"
+	"github.com/iotaledger/wasp/packages/hashing"
 	"github.com/iotaledger/wasp/packages/isc"
+	"github.com/iotaledger/wasp/packages/peering"
 	"github.com/iotaledger/wasp/packages/state"
+	"github.com/iotaledger/wasp/packages/util/pipe"
 )
 
 type stateManager struct {
@@ -23,15 +27,30 @@ type stateManager struct {
 	stateManagerGPA gpa.GPA
 	nodeRandomiser  *smUtils.NodeRandomiser
 	nodeIDToPubKey  map[gpa.NodeID]*cryptolib.PublicKey
+	inputPipe       pipe.Pipe
+	messagePipe     pipe.Pipe
+	net             peering.NetworkProvider
+	netPeeringID    peering.PeeringID
+	ctx             context.Context
+	cleanupFun      func()
 }
 
-var _ consGR.StateMgr = &stateManager{}
-var _ node.ChainStateMgr = &stateManager{}
+var (
+	_ consGR.StateMgr    = &stateManager{}
+	_ node.ChainStateMgr = &stateManager{}
+)
+
+const (
+	constMsgTypeStm    byte = iota
+	constTimerTickTime      = 1 * time.Second
+)
 
 func New(
+	ctx context.Context,
 	chainID *isc.ChainID,
 	me *cryptolib.PublicKey,
 	peerPubKeys []*cryptolib.PublicKey,
+	net peering.NetworkProvider,
 	store kvstore.KVStore,
 	log *logger.Logger,
 ) node.ChainStateMgr { // TODO: consGR.StateMgr {
@@ -42,8 +61,29 @@ func New(
 		chainID:         chainID,
 		stateManagerGPA: smGPA.New(chainID, nr, store, smLog),
 		nodeRandomiser:  nr,
+		inputPipe:       pipe.NewDefaultInfinitePipe(),
+		messagePipe:     pipe.NewDefaultInfinitePipe(),
+		net:             net,
+		netPeeringID:    peering.PeeringIDFromBytes(hashing.HashDataBlake2b(chainID.Bytes(), []byte("STM")).Bytes()),
+		ctx:             ctx,
 	}
 	result.updatePublicKeys(peerPubKeys)
+
+	attachID := result.net.Attach(&result.netPeeringID, peering.PeerMessageReceiverStateManager, func(recv *peering.PeerMessageIn) {
+		if recv.MsgType != constMsgTypeStm {
+			result.log.Warnf("Unexpected message, type=%v", recv.MsgType)
+			return
+		}
+		result.messagePipe.In() <- recv
+	})
+
+	result.cleanupFun = func() {
+		result.inputPipe.Close()
+		result.messagePipe.Close()
+		result.net.Detach(attachID)
+	}
+
+	go result.run()
 	return result
 }
 
@@ -90,7 +130,7 @@ func (smT *stateManager) ConsensusDecidedState(ctx context.Context, aliasOutputI
 // -------------------------------------
 
 func (smT *stateManager) addInput(input gpa.Input) {
-	// TODO
+	smT.inputPipe.In() <- input
 }
 
 func (smT *stateManager) updatePublicKeys(peerPubKeys []*cryptolib.PublicKey) {
@@ -101,6 +141,77 @@ func (smT *stateManager) updatePublicKeys(peerPubKeys []*cryptolib.PublicKey) {
 		smT.nodeIDToPubKey[peerNodeIDs[i]] = peerPubKeys[i]
 	}
 	smT.nodeRandomiser.UpdateNodeIDs(peerNodeIDs)
+}
+
+func (smT *stateManager) run() {
+	defer smT.cleanupFun()
+	ctxCloseCh := smT.ctx.Done()
+	inputPipeCh := smT.inputPipe.Out()
+	messagePipeCh := smT.messagePipe.Out()
+	timerTickCh := time.After(constTimerTickTime)
+	for {
+		select {
+		case input, ok := <-inputPipeCh:
+			if ok {
+				smT.handleInput(input.(gpa.Input))
+			} else {
+				inputPipeCh = nil
+			}
+		case msg, ok := <-messagePipeCh:
+			if ok {
+				smT.handleMessage(msg.(*peering.PeerMessageIn))
+			} else {
+				messagePipeCh = nil
+			}
+		case now, ok := <-timerTickCh:
+			if ok {
+				smT.handleTimerTick(now)
+				timerTickCh = time.After(constTimerTickTime)
+			} else {
+				timerTickCh = nil
+			}
+		case <-ctxCloseCh:
+			smT.log.Debugf("Stopping state manager, because context was closed")
+			return
+		}
+	}
+}
+
+func (smT *stateManager) handleInput(input gpa.Input) {
+	outMsgs := smT.stateManagerGPA.Input(input)
+	smT.sendMessages(outMsgs)
+}
+
+func (smT *stateManager) handleMessage(peerMsg *peering.PeerMessageIn) {
+	msg, err := smT.stateManagerGPA.UnmarshalMessage(peerMsg.MsgData)
+	if err != nil {
+		smT.log.Warnf("Parsing message failed: %v", err)
+		return
+	}
+	msg.SetSender(pubKeyAsNodeID(peerMsg.SenderPubKey))
+	outMsgs := smT.stateManagerGPA.Message(msg)
+	smT.sendMessages(outMsgs)
+}
+
+func (smT *stateManager) handleTimerTick(now time.Time) {
+	smT.handleInput(smInputs.NewStateManagerTimerTick(now))
+}
+
+func (smT *stateManager) sendMessages(msgs gpa.OutMessages) {
+	msgs.MustIterate(func(msg gpa.Message) {
+		msgData, err := msg.MarshalBinary()
+		if err != nil {
+			smT.log.Warnf("Failed to marshal message for sending: %v", err)
+			return
+		}
+		pm := &peering.PeerMessageData{
+			PeeringID:   smT.netPeeringID,
+			MsgReceiver: peering.PeerMessageReceiverStateManager,
+			MsgType:     constMsgTypeStm,
+			MsgData:     msgData,
+		}
+		smT.net.SendMsgByPubKey(smT.nodeIDToPubKey[msg.Recipient()], pm)
+	})
 }
 
 func pubKeyAsNodeID(pubKey *cryptolib.PublicKey) gpa.NodeID {
